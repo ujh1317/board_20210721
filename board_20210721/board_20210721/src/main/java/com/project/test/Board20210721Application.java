c890e8695915b28a47fb5bb1883da99ad27bb721
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-07-21 23:57:29 +0900

    20210721-start

---
+++ board_20210721/board_20210721/src/main/java/com/project/test/Board20210721Application.java
@@ -0,0 +1,13 @@
+package com.project.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board20210721Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board20210721Application.class, args);
+	}
+
+}
---
+++ board_20210721/board_20210721/src/test/java/com/project/test/Board20210721ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board20210721ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
